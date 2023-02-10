66ce6fe7daf72f18b1df47e1262134f881111d93
Iceblok <ycoskun0318@gmail.com>
2023-02-10 16:32:27 +0100

    les2

---
+++ 1b.py
@@ -0,0 +1,19 @@
+# een code die de solution (geheime code) bepaald met behulp van random.
+
+# een code die aangeeft als een gekozen kleur in de solution zit, en of hij ook op de juiste plek zit
+# ja of nee (wit = juiste kleur geschat, zwart = juiste kleur op de juiste plek geschat)
+
+# check functie aanmaken waarbij je jou gok meegeeft.
+
+# delete functie
+
+# new game functie
+
+# game rules
+
+# vraag = wat je gokt
+# antwoord = witten / zwarte ballen
+
+# een functie die aangeeft of je hebt gewonnen of niet (afsluiten)
+
+# simple solution: maak een lijst met alle combinaties,
